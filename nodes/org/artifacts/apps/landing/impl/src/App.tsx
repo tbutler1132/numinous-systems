a5e8815f1e89dff14fef67f7cc0f056b77f22e7a
Tim Butler <tbutler1132@gmail.com>
2026-01-18 17:37:56 -0500

    remove the ear stuff

--- nodes/org/artifacts/apps/landing/impl/src/App.tsx
+++ nodes/org/artifacts/apps/landing/impl/src/App.tsx
@@ -3,7 +3,7 @@ export default function App() {
     <div className="landing">
       <div className="landing-content">
         <header className="landing-header">
-          <h1 className="landing-name">Timothy Butluneaar</h1>
+          <h1 className="landing-name">Timothy Butler</h1>
         </header>
 
         <nav className="landing-nav">
